cbde662a0590fb7a7667874ecd836e468674452a
root <root@DESKTOP-31HM7L6.localdomain>
2023-05-09 06:46:45 -0400

    Updating server.js, updated syntax issue after var file

--- server.js
+++ server.js
@@ -47,8 +47,7 @@ const server = http.createServer(function(req, response) {
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
-server.listen(port, () => {
-    console.log('Server listening on port ${port}');
-});
+server.listen(port);
+console.log(`Server listening on port ${port}`);
 
 // That's it! You're all done!
